bf36419409acc381d5e3a07a02d5dda9517b7bae
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2021-07-13 08:06:52 +0700

    Custom Login Form

--- customlogin/src/main/java/com/example/customlogin/CustomloginApplication.java
+++ customlogin/src/main/java/com/example/customlogin/CustomloginApplication.java
@@ -2,6 +2,8 @@ package com.example.customlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class CustomloginApplication {
@@ -10,4 +12,13 @@ public class CustomloginApplication {
         SpringApplication.run(CustomloginApplication.class, args);
     }
 
+    @GetMapping("/users")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World")String name){
+        return String.format("Users Hello %s!", name);
+    }
+
+    @GetMapping("/admin")
+    public String sayAdmin(@RequestParam(value = "myName", defaultValue = "World")String name){
+        return String.format("Admin Hello %s!", name);
+    }
 }
